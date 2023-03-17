9c62061382b47d0baed043b0b5ee13cd95f5e1a8
jprajwal <prajwaltj@gmail.com>
2023-03-17 09:44:25 +0530

    auth response implementation slightly tested and refactored

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 #![allow(dead_code)]
 
+use serde::{self, Deserialize, Serialize};
+use serde_json;
+use serde_urlencoded;
 use std::collections::HashMap;
-use ureq;
-use url::{ParseError, Url};
 
 #[derive(Default, Debug, PartialEq, Eq)]
 struct AuthCodeRequest {
@@ -61,26 +62,39 @@ impl AuthCodeRequest {
         self
     }
 
-    fn get_url(&self) -> Result<String, ParseError> {
-        let mut url = Url::parse(self.auth_url.as_str())?;
-        let mut queries = url.query_pairs_mut();
-        queries
-            .append_pair("client_id", self.client_id.as_str())
-            .append_pair("response_type", self.response_type.as_str());
+    fn get_url(&self) -> Result<String, ()> {
+        let mut url: String = self.auth_url.clone();
+
+        let result: String;
+        let mut params = vec![
+            ("client_id", self.client_id.as_str()),
+            ("response_type", self.response_type.as_str()),
+        ];
         if let Some(ref redirect_url) = self.redirect_url {
-            queries.append_pair("redirect_uri", redirect_url.as_str());
+            params.push(("redirect_uri", redirect_url.as_str()));
         }
         if let Some(ref state) = self.state {
-            queries.append_pair("state", state.as_str());
+            params.push(("state", state.as_str()));
         }
         if let Some(ref scopes) = self.scope {
-            let result = utils::join(scopes.iter().map(|s| s.as_str()), ' ');
-            queries.append_pair("scope", result.as_str());
+            result = utils::join(scopes.iter().map(|s| s.as_str()), ' ');
+            params.push(("scope", result.as_str()));
         }
         if let Some(ref extras) = self.extras {
-            queries.extend_pairs(extras);
+            params.extend(
+                extras
+                    .iter()
+                    .map(|(a, b)| (a.as_str(), b.as_str()))
+                    .collect::<Vec<_>>(),
+            );
         }
-        let url = queries.finish();
+
+        url.push('?');
+        url.push_str(
+            serde_urlencoded::to_string(params)
+                .map_err(|_| ())?
+                .as_str(),
+        );
         Ok(url.to_string())
     }
 }
@@ -129,56 +143,50 @@ impl AuthCodeAccessTokenRequest {
         self
     }
 
-    fn into_url(&mut self) -> Result<String, ParseError> {
-        let mut url = Url::parse(self.token_url.as_str())?;
-        let url = url.query_pairs_mut().clear().finish();
-        return Ok(url.to_string());
+    fn token_url(&mut self) -> Result<String, ()> {
+        // TODO: Validate URL
+        return Ok(self.token_url.clone());
     }
 
-    fn get_token(&mut self) -> Result<AuthCodeToken, ()> {
-        let url = self.into_url().map_err(|_| ())?;
-        let mut form_data = vec![
+    fn req_body(&mut self) -> Result<String, ()> {
+        let mut params = vec![
             ("grant_type", self.grant_type.as_str()),
             ("code", self.code.as_str()),
         ];
         if let Some(ref redirect_url) = self.redirect_url {
-            form_data.push(("redirect_uri", redirect_url.as_str()));
+            params.push(("redirect_uri", redirect_url.as_str()));
         }
         if let Some(ref client_id) = self.client_id {
-            form_data.push(("client_id", client_id.as_str()));
+            params.push(("client_id", client_id.as_str()));
         }
         if let Some(ref client_secret) = self.client_secret {
-            form_data.push(("client_secret", client_secret.as_str()));
+            params.push(("client_secret", client_secret.as_str()));
         }
-        let response: HashMap<String, String> = ureq::post(url.as_str())
-            .send_form(form_data.as_slice())
-            .map_err(|_| ())
-            .and_then(|resp| match resp.status() {
-                200 => resp.into_json().map_err(|_| ()),
-                _ => Err(()),
-            })?;
+        let body = serde_urlencoded::to_string(&params).map_err(|_| ())?;
+        Ok(body)
+    }
 
-        let mut token = AuthCodeToken::new(
-            response
-                .get("access_token")
-                .map_or(String::default(), |s| s.to_owned()),
-            response
-                .get("token_type")
-                .map_or(String::default(), |s| s.to_owned()),
-        );
-        if let Some(refresh_token) = response.get("refresh_token") {
-            token = token.set_refresh_token(refresh_token.to_owned());
-        }
-        if let Some(expires_in) = response.get("expires_in") {
-            token = token.set_exprires_in(expires_in.parse().unwrap_or(u32::default()));
-        }
-        if let Some(ref scopes) = response.get("scope") {
-            token = token.set_scope(scopes.split(' ').map(|s| s.to_owned()).collect());
-        }
+    fn get_token<T: PostRequest>(&mut self, requester: T) -> Result<AuthCodeToken, ()> {
+        let url = self.token_url().map_err(|_| ())?;
+        let form_data = self.req_body().map_err(|_| ())?;
+        let response = requester.post(url, form_data).map_err(|_| ())?;
+        let token: AuthCodeToken = serde_json::from_str(response.as_str()).map_err(|_| ())?;
         return Ok(token);
     }
 }
 
+trait Token {
+    fn set_refresh_token(self, refresh_token: String) -> Self;
+    fn set_exprires_in(self, expires_in: u32) -> Self;
+    fn set_scope(self, scopes: Vec<String>) -> Self;
+    fn access_token(&self) -> String;
+    fn refresh_token(&self) -> Option<String>;
+    fn token_type(&self) -> String;
+    fn scopes(&self) -> Option<Vec<String>>;
+    fn is_valid(&self) -> bool;
+}
+
+#[derive(Debug, Eq, PartialEq, Deserialize, Serialize)]
 struct AuthCodeToken {
     access_token: String,
     token_type: String,
@@ -187,37 +195,7 @@ struct AuthCodeToken {
     scope: Option<Vec<String>>,
 }
 
-/*
-impl Deserialize for AuthCodeToken {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: ureq::serde::Deserializer<'de>,
-    {
-        deserializer.deserialize_struct(
-            "AuthCodeToken",
-            &[
-                "access_token",
-                "token_type",
-                "refresh_token",
-                "expires_in",
-                "scope",
-            ],
-        )
-    }
-}
- */
-
-impl AuthCodeToken {
-    fn new(access_token: String, token_type: String) -> Self {
-        AuthCodeToken {
-            access_token,
-            token_type,
-            refresh_token: None,
-            expires_in: None,
-            scope: None,
-        }
-    }
-
+impl Token for AuthCodeToken {
     fn set_refresh_token(mut self, refresh_token: String) -> Self {
         self.refresh_token = Some(refresh_token);
         self
@@ -232,6 +210,42 @@ impl AuthCodeToken {
         self.scope = Some(scopes);
         self
     }
+
+    fn access_token(&self) -> String {
+        self.access_token.clone()
+    }
+
+    fn refresh_token(&self) -> Option<String> {
+        self.refresh_token.clone()
+    }
+
+    fn token_type(&self) -> String {
+        self.token_type.clone()
+    }
+
+    fn scopes(&self) -> Option<Vec<String>> {
+        self.scope.clone()
+    }
+
+    fn is_valid(&self) -> bool {
+        true
+    }
+}
+
+impl AuthCodeToken {
+    fn new(access_token: String, token_type: String) -> Self {
+        AuthCodeToken {
+            access_token,
+            token_type,
+            refresh_token: None,
+            expires_in: None,
+            scope: None,
+        }
+    }
+}
+
+trait PostRequest {
+    fn post(&self, url: String, body: String) -> Result<String, ()>;
 }
 
 mod utils {
@@ -321,5 +335,35 @@ mod tests {
             .set_redirect_url("https://oauth2.example.com/code".into());
         let url = request.get_url().unwrap_or("".into());
         println!("url: {url}");
+    }
+
+    #[derive(Debug)]
+    struct TestPostRequester {
+        response: String,
+    }
+
+    impl PostRequest for TestPostRequester {
+        fn post(&self, _url: String, _body: String) -> Result<String, ()> {
+            Ok(self.response.clone())
+        }
+    }
+
+    #[test]
+    fn test_auth_code_get_token() {
+        let requester = TestPostRequester {
+            response: String::from(
+                r#"{"access_token": "test_token", "refresh_token": "test_refresh_token", "expires_in": 3600, "token_type": "Bearer"}"#,
+            ),
+        };
+        let mut token_request =
+            AuthCodeAccessTokenRequest::new("test_url".into(), "code".into(), "test_code".into());
+        let expected_token = AuthCodeToken {
+            access_token: "test_token".into(),
+            token_type: "Bearer".into(),
+            refresh_token: Some("test_refresh_token".into()),
+            expires_in: Some(3600),
+            scope: None,
+        };
+        assert_eq!(token_request.get_token(requester), Ok(expected_token));
     }
 }
