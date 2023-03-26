101bcf3133ed4a9b54555e30d418829e3064f149
jprajwal <prajwaltj@gmail.com>
2023-03-26 23:32:27 +0530

    implemented deserialization of scopes

--- src/lib.rs
+++ src/lib.rs
@@ -133,6 +133,11 @@ impl AccessTokenRequest {
         self
     }
 
+    pub fn set_code(mut self, code: String) -> Self {
+        self.code = Some(code);
+        self
+    }
+
     pub fn set_redirect_url(mut self, redirect_url: String) -> Self {
         self.redirect_url = Some(redirect_url);
         self
@@ -218,7 +223,7 @@ pub struct AuthCodeToken {
     token_type: String,
     refresh_token: Option<String>,
     expires_in: Option<u32>,
-    #[serde(skip)] // TODO: parse scopes into vector
+    #[serde(deserialize_with = "utils::deserialize_space_sep_vec")]
     scope: Option<Vec<String>>,
     #[serde(skip, default = "SystemTime::now")]
     generated_time: SystemTime,
@@ -452,6 +457,7 @@ impl Display for AuthTokenError {
 }
 
 mod utils {
+    use serde::{Deserialize, Deserializer};
     pub fn join<'a, I, T>(mut str_iter: I, sep: char) -> String
     where
         I: Iterator<Item = &'a T>,
@@ -466,6 +472,25 @@ mod utils {
             result.push_str(chunk.as_ref());
         });
         return result;
+    }
+
+    pub fn deserialize_space_sep_vec<'de, D>(d: D) -> Result<Option<Vec<String>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        use serde::de::Error;
+        use serde_json::Value;
+        if let Some(space_delimited) = Option::<String>::deserialize(d)? {
+            let entries = space_delimited
+                .split(' ')
+                .map(|s| Value::String(s.to_string()))
+                .collect();
+            let res = Vec::<String>::deserialize(Value::Array(entries)).map_err(Error::custom)?;
+            Ok(Some(res))
+        } else {
+            // If the JSON value is null, use the default value.
+            Ok(Some(Vec::default()))
+        }
     }
 }
 
